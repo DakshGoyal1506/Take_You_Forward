0e2a85b2be526af3713e4c61845dacdbd2c9ac85
Daksh Goyal <daksh.goyal1506@gmail.com>
2025-03-10 21:05:53 +0530

    9-10/3/2025

---
+++ leetcode/796_Rotate_String.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool rotateString(string s, string goal) {
+
+            if (s.length() != goal.length())
+            {
+                return false;
+            }
+
+            string a = s + s;
+
+            return a.find(goal) != string::npos;
+            
+        }
+    };
