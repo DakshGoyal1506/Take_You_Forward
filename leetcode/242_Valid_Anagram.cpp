9d6de6c3d9217e4b3fd073a7a43bfa213122802f
Daksh Goyal <daksh.goyal1506@gmail.com>
2025-06-04 21:16:08 +0530

    4/6/2025

--- leetcode/242_Valid_Anagram.cpp
+++ leetcode/242_Valid_Anagram.cpp
@@ -1,23 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// class Solution {
+//     public:
+//         bool isAnagram(string s, string t) {
+
+//             if(s.length() != t.length())
+//             {
+//                 return false;
+//             }
+
+//             sort(s.begin(), s.end());
+//             sort(t.begin(), t.end());
+
+//             int n = s.length();
+
+//             for(int i = 0; i < n; i++)
+//             {
+//                 if(s[i] != t[i])
+//                 {
+//                     return false;
+//                 }
+//             }
+
+//             return true;
+            
+//         }
+//     };
+
 class Solution {
     public:
         bool isAnagram(string s, string t) {
 
-            if(s.length() != t.length())
+            unordered_map<char, int> freq;
+
+            for(char ch1 : s)
             {
-                return false;
+                freq[ch1] = freq[ch1] + 1;
             }
 
-            sort(s.begin(), s.end());
-            sort(t.begin(), t.end());
-
-            int n = s.length();
-
-            for(int i = 0; i < n; i++)
+            for(char ch2 : t)
             {
-                if(s[i] != t[i])
+                freq[ch2] = freq[ch2] - 1;
+            }
+
+            for(auto &pair : freq)
+            {
+                if(pair.second != 0)
                 {
                     return false;
                 }
---
+++ leetcode/28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int strStr(string haystack, string needle) {
+
+            int index = haystack.find(needle);
+
+            if(index != string::npos) return index;
+            else return -1;
+        }
+    };
